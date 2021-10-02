00d386646609e6afae478b16d7e01149fc2db940
Jan Pecquet <jan.pecquet@outlook.com>
2021-10-02 23:16:35 +0200

    Addd animal feature card :dog:

---
+++ lib/src/views/animal_detail_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AnimalDetail extends StatelessWidget {
+  const AnimalDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
