de02fd086656596160d6622417404a9548927dea
Jan Pecquet <jan.pecquet@outlook.com>
2021-10-02 21:26:16 +0200

    Set preferred orientation to portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -6,6 +7,7 @@ import 'src/settings/settings_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   final settingsController = SettingsController(SettingsService());
   await settingsController.loadSettings();
