21e2e82dfd3d8c3f94d913519cac637e466d683b
Kristof Dewilde <code@qrivi.dev>
2021-12-31 06:52:49 +0100

    Don't mind me. Just super easily setting and reading state in the UI. 😎

--- lib/views/application.dart
+++ lib/views/application.dart
@@ -1,64 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:rescado/controllers/settings_controller.dart';
 
-// 100% placeholder application while I figure out the domain.
-class Application extends StatelessWidget {
+// Quick demo to test controllers. The trick is to use ConsumerWidget or HookConsumerWidget once we start animating.
+class Application extends ConsumerWidget {
   const Application({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Riverpod Test',
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      // To read a controller, just watch() it and access the fields on the state model. ☺️
+      themeMode: ref.watch(settingsControllerProvider).themeMode,
+      home: const ProviderTest(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class ProviderTest extends ConsumerWidget {
+  const ProviderTest({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Provider Test'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text('Cmd+Shift+A om snel System appearance te togglen in de iOS simulator. 👍'),
+              const SizedBox(height: 75.0),
+              // To write to a controller, read it's notifier and you have access to its state altering methods. ☺️
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      ref.read(settingsControllerProvider.notifier).setThemeMode(ThemeMode.system);
+                    },
+                    child: const Text('System'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      ref.read(settingsControllerProvider.notifier).setThemeMode(ThemeMode.light);
+                    },
+                    child: const Text('Light'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      ref.read(settingsControllerProvider.notifier).setThemeMode(ThemeMode.dark);
+                    },
+                    child: const Text('Dark'),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
