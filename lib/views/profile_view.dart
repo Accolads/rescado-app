0d93dc36b631b50ab72d54125693de12dee91606
Kristof Dewilde <k.dewilde@napoleongames.be>
2022-01-05 21:50:08 +0100

    Reinstated PageTitle and added FullPage to use it

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:rescado/views/containers/full_page.dart';
 
 // Placeholder view.
 class ProfileView extends StatelessWidget {
   static const viewId = 'ProfileView';
 
-  const ProfileView({Key? key}) : super(key: key);
+  const ProfileView({
+    Key? key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => const Scaffold(
+  Widget build(BuildContext context) => FullPage(
+        title: AppLocalizations.of(context)!.labelProfile,
         body: Center(
-          child: Text(
-            'Placeholder ProfileView',
+          child: Column(
+            children: const <Widget>[
+              SizedBox(height: 500.0),
+              Text('Placeholder ProfileView'),
+              SizedBox(height: 500.0),
+            ],
           ),
         ),
       );
