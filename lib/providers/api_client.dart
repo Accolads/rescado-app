2db9e8e946f79e9257b26d230f2c0b2e5b3eacbd
Kristof Dewilde <k.dewilde@napoleongames.be>
2022-01-05 17:50:54 +0100

    Fix: perform request inside try-catch to actually catch exceptions

--- lib/providers/api_client.dart
+++ lib/providers/api_client.dart
@@ -63,27 +63,27 @@ class ApiClient extends http.BaseClient {
 
   // BaseClient.get(), but with response processing and error handling.
   Future<Map<String, dynamic>> getJson(Uri url, {Map<String, String>? headers}) async => _parseResponse(
-        await super.get(url, headers: await _authIfNeeded(url, headers)).timeout(RescadoConstants.timeout),
+        () async => await super.get(url, headers: await _authIfNeeded(url, headers)).timeout(RescadoConstants.timeout),
       );
 
   // BaseClient.post(), but with response processing and error handling.
   Future<Map<String, dynamic>> postJson(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) async => _parseResponse(
-        await super.post(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
+        () async => await super.post(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
       );
 
   // BaseClient.put(), but with response processing and error handling.
   Future<Map<String, dynamic>> putJson(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) async => _parseResponse(
-        await super.put(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
+        () async => await super.put(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
       );
 
   // BaseClient.patch(), but with response processing and error handling.
   Future<Map<String, dynamic>> patchJson(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) async => _parseResponse(
-        await super.patch(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
+        () async => await super.patch(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
       );
 
   // BaseClient.delete(), but with response processing and error handling.
   Future<Map<String, dynamic>> deleteJson(Uri url, {Map<String, String>? headers, Object? body, Encoding? encoding}) async => _parseResponse(
-        await super.delete(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
+        () async => await super.delete(url, headers: await _authIfNeeded(url, headers), body: body, encoding: encoding).timeout(RescadoConstants.timeout),
       );
 
   // If the url is not a public url, add the authorization header.
@@ -96,8 +96,9 @@ class ApiClient extends http.BaseClient {
   }
 
   // Function to parse the JSON returned by the server as soon as the response gets in. Also handles errors thrown by the API and wraps other exceptions if thrown.
-  Map<String, dynamic> _parseResponse(http.Response response) {
+  Future<Map<String, dynamic>> _parseResponse(Function request) async {
     try {
+      final http.Response response = (await request()) as http.Response;
       // Try to parse the JSON response and wrap it in an exception to shortcut the flow if applicable.
       var body = jsonDecode(response.body) as Map<String, dynamic>;
       if (body.containsKey('errors')) {
@@ -117,8 +118,7 @@ class ApiClient extends http.BaseClient {
       throw const OfflineException();
     } on FormatException {
       // FormatException is thrown by jsonDecode() if it cannot parse the string passed to it (indicating the server did not respond with JSON, which it always should).
-      _logger.e('The server did not respond with JSON data!');
-      _logger.d(response.body);
+      _logger.w('The server did not respond with JSON data!');
       throw const ServerException();
     } catch (error, stackTrace) {
       // When something is very wrong.
