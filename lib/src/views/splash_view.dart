85d594e47be566808f0d88544849129d3d0d0ebe
Kristof Dewilde <k.dewilde@napoleongames.be>
2022-01-11 13:50:21 +0100

    Remove redundant consumer

--- lib/src/views/splash_view.dart
+++ lib/src/views/splash_view.dart
@@ -44,67 +44,61 @@ class _SplashViewState extends ConsumerState<SplashView> with SingleTickerProvid
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: double.infinity,
-        color: ref.watch(settingsControllerProvider).activeTheme.accentColor,
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            AnimatedBuilder(
-              animation: _animationController,
-              builder: (_, __) => SizedBox(
-                width: 125.0,
-                child: Stack(
-                  children: <Widget>[
-                    SvgPicture.asset('assets/logo/heart.svg'),
-                    ...[1, 2, 3, 4].map((i) {
-                      var value = _animationController.value * 50;
-                      var y = (value > i * 10 && value < i * 10 + 10) ? value - i * 10 : 0.0;
-                      // Not the animation I wanted but doesn't look too bad lol, so works for now ✌️
-                      return Transform.translate(
-                        offset: Offset(0, -y),
-                        child: SvgPicture.asset('assets/logo/toe$i.svg'),
-                      );
-                    }),
-                  ],
+  Widget build(BuildContext context) => Scaffold(
+        body: Container(
+          width: double.infinity,
+          color: ref.watch(settingsControllerProvider).activeTheme.accentColor,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              AnimatedBuilder(
+                animation: _animationController,
+                builder: (_, __) => SizedBox(
+                  width: 125.0,
+                  child: Stack(
+                    children: <Widget>[
+                      SvgPicture.asset('assets/logo/heart.svg'),
+                      ...[1, 2, 3, 4].map((i) {
+                        var value = _animationController.value * 50;
+                        var y = (value > i * 10 && value < i * 10 + 10) ? value - i * 10 : 0.0;
+                        // Not the animation I wanted but doesn't look too bad lol, so works for now ✌️
+                        return Transform.translate(
+                          offset: Offset(0, -y),
+                          child: SvgPicture.asset('assets/logo/toe$i.svg'),
+                        );
+                      }),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Consumer(
-              builder: (_, WidgetRef ref, __) {
-                return ref.watch(userControllerProvider).when(
-                    data: (User user) {
-                      var nextViewId = (user.status == UserStatus.anonymous || user.status == UserStatus.identified) ? MainView.viewId : AuthenticationView.viewId;
-                      // Future.delayed(Duration.zero) is a bit too hacky to my liking but we need to return a widget and using Navigator otherwise shortcuts the flow, resulting in errors.
-                      Future.delayed(Duration.zero, () => Navigator.pushReplacementNamed(context, nextViewId));
-                      return _buildPlaceholder();
-                    },
-                    error: (Object error, _) {
-                      _animationController.reset();
-                      final isOffline = error is OfflineException;
+              ref.watch(userControllerProvider).when(
+                  data: (User user) {
+                    var nextViewId = (user.status == UserStatus.anonymous || user.status == UserStatus.identified) ? MainView.viewId : AuthenticationView.viewId;
+                    // Future.delayed(Duration.zero) is a bit too hacky to my liking but we need to return a widget and using Navigator otherwise shortcuts the flow, resulting in errors.
+                    Future.delayed(Duration.zero, () => Navigator.pushReplacementNamed(context, nextViewId));
+                    return _buildPlaceholder();
+                  },
+                  error: (Object error, _) {
+                    _animationController.reset();
+                    final isOffline = error is OfflineException;
 
-                      return ActionCard(
-                        title: isOffline ? AppLocalizations.of(context)!.errorTitleAuthOffline : AppLocalizations.of(context)!.errorTitleAuthGeneric,
-                        body: isOffline ? AppLocalizations.of(context)!.errorBodyAuthOffline : AppLocalizations.of(context)!.errorBodyAuthGeneric,
-                        animated: true,
-                        svgAsset: RescadoConstants.illustrationWomanWithWrench,
-                        actionButton: ActionButton(
-                          label: AppLocalizations.of(context)!.labelRetry,
-                          svgAsset: RescadoConstants.iconRefresh,
-                          onPressed: () => ref.read(userControllerProvider.notifier).renewSession(),
-                        ),
-                      );
-                    },
-                    loading: () => _buildPlaceholder());
-              },
-            ),
-          ],
+                    return ActionCard(
+                      title: isOffline ? AppLocalizations.of(context)!.errorTitleAuthOffline : AppLocalizations.of(context)!.errorTitleAuthGeneric,
+                      body: isOffline ? AppLocalizations.of(context)!.errorBodyAuthOffline : AppLocalizations.of(context)!.errorBodyAuthGeneric,
+                      animated: true,
+                      svgAsset: RescadoConstants.illustrationWomanWithWrench,
+                      actionButton: ActionButton(
+                        label: AppLocalizations.of(context)!.labelRetry,
+                        svgAsset: RescadoConstants.iconRefresh,
+                        onPressed: () => ref.read(userControllerProvider.notifier).renewSession(),
+                      ),
+                    );
+                  },
+                  loading: () => _buildPlaceholder()),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
 
   Widget _buildPlaceholder() {
     _animationController.repeat(reverse: true);
