1f49cefb0007b83dae3c2c11f14de5c480396fb3
Kristof Dewilde <k.dewilde@napoleongames.be>
2022-01-23 09:25:59 +0100

    Merged card and like repo, split user controller into auth and account controller
    # Conflicts:
    #	lib/src/data/models/user.dart
    #	lib/src/data/repositories/card_repository.dart
    #	lib/src/data/repositories/like_repository.dart
    #	lib/src/services/controllers/authentication_controller.dart

--- lib/src/data/models/authentication.dart
+++ lib/src/data/models/authentication.dart
@@ -1,13 +1,23 @@
 import 'package:rescado/src/data/models/account.dart';
+import 'package:rescado/src/utils/extensions.dart';
+
+enum AuthenticationStatus {
+  anonymous, // when connected with token linked to an anonymous account
+  identified, // when connected with token linked to an account with user data
+  expired, // when attempted to connect with a token that was expired and the session could not be recovered
+  blocked, // when attempted to connect with a token linked to a blocked account
+}
 
 class Authentication {
-  final AccountStatus status;
+  final AuthenticationStatus status;
 
   Authentication._({
     required this.status,
   });
 
+  factory Authentication.expired() => Authentication._(status: AuthenticationStatus.expired);
+
   factory Authentication.fromJson(Map<String, dynamic> json) => Authentication._(
-        status: AccountStatus.values.byName((json['status'] as String).toLowerCase()),
+        status: AccountStatus.values.byName((json['status'] as String).toLowerCase()).toAuthenticationStatus(),
       );
 }
--- lib/src/services/controllers/authentication_controller.dart
+++ lib/src/services/controllers/authentication_controller.dart
@@ -1,23 +1,21 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rescado/src/data/models/account.dart';
-import 'package:rescado/src/data/models/user.dart';
-import 'package:rescado/src/data/repositories/account_repository.dart';
+import 'package:rescado/src/data/models/authentication.dart';
 import 'package:rescado/src/data/repositories/authentication_repository.dart';
 import 'package:rescado/src/exceptions/api_exception.dart';
 import 'package:rescado/src/services/providers/device_storage.dart';
-import 'package:rescado/src/utils/extensions.dart';
 import 'package:rescado/src/utils/logger.dart';
 
-final userControllerProvider = StateNotifierProvider<UserController, AsyncValue<User>>(
-  (ref) => UserController(ref.read).._initialize(),
+final authenticationControllerProvider = StateNotifierProvider<AuthenticationController, AsyncValue<Authentication>>(
+  (ref) => AuthenticationController(ref.read).._initialize(),
 );
 
-class UserController extends StateNotifier<AsyncValue<User>> {
-  static final _logger = addLogger('UserController');
+class AuthenticationController extends StateNotifier<AsyncValue<Authentication>> {
+  static final _logger = addLogger('AuthenticationController');
 
   final Reader _read;
 
-  UserController(this._read) : super(const AsyncValue.loading());
+  AuthenticationController(this._read) : super(const AsyncValue.loading());
 
   void _initialize() async {
     _logger.d('initialize()');
@@ -38,16 +36,19 @@ class UserController extends StateNotifier<AsyncValue<User>> {
       if (token == null) {
         // TODO Do we want to consider, in the future, offering the user the option to log in before automatically creating an account?
         _logger.i('Creating a new account for a first-time user.');
-        await _read(authenticationRepositoryProvider).register();
+        final authentication = await _read(authenticationRepositoryProvider).register();
 
         _logger.i('User registration was successful.');
-        state = AsyncValue.data(await _fetchUserData());
+        state = AsyncValue.data(authentication);
         return;
       }
 
       try {
         _logger.i('Attempting to refresh the session using the refresh token in the JWT.');
-        await _read(authenticationRepositoryProvider).refresh();
+        final authentication = await _read(authenticationRepositoryProvider).refresh();
+
+        _logger.i('Session renewal was successful.');
+        state = AsyncValue.data(authentication);
       } on ApiException catch (exception) {
         if (exception.keys.first == 'TokenExpired') {
           _logger.w('The session is expired.');
@@ -57,7 +58,7 @@ class UserController extends StateNotifier<AsyncValue<User>> {
             await _read(authenticationRepositoryProvider).recover();
           } else {
             _logger.i('User was not anonymous. Will need to log in manually to continue.');
-            state = AsyncValue.data(User(status: UserStatus.expired));
+            state = AsyncValue.data(Authentication.expired());
             return;
           }
         } else {
@@ -65,9 +66,6 @@ class UserController extends StateNotifier<AsyncValue<User>> {
           rethrow;
         }
       }
-
-      _logger.i('Session renewal was successful.');
-      state = AsyncValue.data(await _fetchUserData());
     } catch (error, stackTrace) {
       // This is purely UX. Hitting the retry button must make it look like something is happening too, even if the result is instant.
       await Future<dynamic>.delayed(const Duration(milliseconds: 3333));
@@ -75,15 +73,5 @@ class UserController extends StateNotifier<AsyncValue<User>> {
       _logger.e('Session renewal failed.', error, stackTrace);
       state = AsyncValue.error(error);
     }
-  }
-
-  Future<User> _fetchUserData() async {
-    _logger.i('Fetching the current user\'s account data.');
-    final accountData = await _read(accountRepositoryProvider).getAccount();
-
-    return User(
-      status: accountData.status.toUserStatus(),
-      account: accountData,
-    );
   }
 }
