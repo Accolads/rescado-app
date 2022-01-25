d439f7b3fa6b9e3f92488a85f186833ff409840d
Kristof Dewilde <code@qrivi.dev>
2022-01-25 02:35:17 +0100

    Always send ApiException and ServerException to Crashlytics

--- lib/src/exceptions/api_exception.dart
+++ lib/src/exceptions/api_exception.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
 class ApiException implements Exception {
   late final List<String> _messages;
   late final List<String> _keys;
@@ -9,6 +11,12 @@ class ApiException implements Exception {
   ApiException(List<String> messages) {
     _messages = messages;
     _keys = messages.map<String>((message) => message.substring(1, message.indexOf(']'))).toList();
+
+    FirebaseCrashlytics.instance.recordError(
+      this,
+      StackTrace.current,
+      reason: 'ApiException',
+    );
   }
 
   @override
