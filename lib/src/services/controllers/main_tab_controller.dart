a3e45fb8d052caaa93d6274307f052563d1c93b7
Kristof Dewilde <k.dewilde@napoleongames.be>
2022-03-13 05:03:17 +0100

    Removed unused references

--- lib/src/services/controllers/main_tab_controller.dart
+++ lib/src/services/controllers/main_tab_controller.dart
@@ -1,19 +1,15 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rescado/src/constants/rescado_constants.dart';
-import 'package:rescado/src/services/controllers/like_controller.dart';
 import 'package:rescado/src/utils/logger.dart';
-import 'package:rescado/src/views/profile_view.dart';
 
 final tabControllerProvider = StateNotifierProvider<MainTabController, int>(
-  (ref) => MainTabController(ref.read),
+  (_) => MainTabController(),
 );
 
 class MainTabController extends StateNotifier<int> {
   static final _logger = addLogger('MainTabController');
 
-  final Reader _read;
-
-  MainTabController(this._read) : super(RescadoConstants.mainViewInitialTab);
+  MainTabController() : super(RescadoConstants.mainViewInitialTab);
 
   void setActiveTab(int tab) {
     _logger.d('setActiveTab()');
